df0f37ec7fa382260cf333cc2db1e4eeb0c3ad3c
nguyen duy dien <nguyenduydien1311999@gmail.com>
2019-07-11 10:05:10 +0700

    refactor contract code

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -16,25 +16,26 @@ contract Proxy {
     require(_impl != address(0));
 
     assembly {
-      let ptr := mload(0x40) // trỏ ptr tới địa chỉ bắt đầu là  0x40 , ox40 là địa chỉ đặc biệt của solidity , nó trỏ tới cùng nhớ mới sau vùng nhớ đã lưu
+      let ptr := mload(0x40)
 
-      // calldatasize: size of the cal data
-      // copy 'calldatasize' bytes from position 0 to memory at ptr
+      // (1) copy incoming call data
       calldatacopy(ptr, 0, calldatasize)
-      let result := delegatecall(gas, _impl, ptr, calldatasize, 0, 0)
-      // call contract at address _impl with input mem(ptr..(ptr+calldatasize)) providing 'gas' gas and output area mem[0..0]
-      // out and outsize are 0 because we don't know the size yet.
-      // return 0 (error) or 1 (success)
-      let size := returndatasize
-      returndatacopy(ptr, 0, size) // copy 'size' bytes from returndata at position '0' to mem at position 'ptr'
 
+      // (2) forward call to logic contract
+      let result := delegatecall(gas, _impl, ptr, calldatasize, 0, 0)
+      let size := returndatasize
+
+      // (3) retrieve return data
+      returndatacopy(ptr, 0, size)
+
+      // (4) forward return data back to caller
       switch result
       case 0 {
-        revert(ptr, size) // end execution, revert state changes, return data mem[p..(p+s)) (data from 'ptr' to 'size')
-      }
+        revert(ptr, size)
+        }
       default {
-        return(ptr, size) // end execution, return data mem[p..(p+s))
-      }
+        return(ptr, size)
+        }
     }
   }
 }
